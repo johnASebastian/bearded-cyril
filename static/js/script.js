b9e1004a5a96b021cf30eb6080d2abdb78bf8615
David Cox <losttime.shuffle@gmail.com>
2014-10-07 23:24:20 +0000

    Add static site files

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', startup, false);
+
+var startup = function() {
+
+	var zones = document.querySelector('div.zones');
+
+	zones.forEach(function(zoneElement, index) {
+		
+	});
+
+};
+
+var Zone = function() {
+};
+
+Zone.prototype.
