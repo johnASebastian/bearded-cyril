703ec4ad3a2c0c1f829597843d49b7fe2ad6df45
John Sebastian <jsebastian@getjive.com>
2014-10-07 15:29:01 -0600

    changes to manager

--- python/ZoneManager.py
+++ python/ZoneManager.py
@@ -3,10 +3,10 @@ import time
 
 class ZoneManager(object):
 	zones = [11,7,16,10]
-	
-	def setupZones(self):
+
+	def __init__ (self):
 		GPIO.setmode(GPIO.BOARD)
-		for or pin in zones:
+		for pin in zones:
 			GPIO.setup(pin, GPIO.OUT)
 
 	def turnZoneOn(self, zoneNumber):
