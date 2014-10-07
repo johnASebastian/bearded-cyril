af50e3bfb83a0d8bfc4a487eea40f9cac5633c6e
John Sebastian <jsebastian@getjive.com>
2014-10-07 14:39:32 -0600

    set gpio pins to output

--- python /setupGpio.py
+++ python /setupGpio.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
-for pin in range(2,26):
-	GPIO.setup(x, GPIO.OUT)
+zones = [11,7,16,10]
+for pin in zones:
+	GPIO.setup(pin, GPIO.OUT)
 
-	
