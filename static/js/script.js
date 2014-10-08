01b13bdbda281145231f284b13bbd17167e0f3bd
David Cox <dcox@getjive.com>
2014-10-07 18:15:07 -0600

    Multiple zones with functional buttons

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,62 @@
-document.addEventListener('DOMContentLoaded', startup, false);
+'use strict';
 
-var startup = function() {
+(function() {
+	var zones = document.querySelectorAll('div.zone');
 
-	var zones = document.querySelector('div.zones');
+	var Zone = function(element) {
+		var zone = this;
+		console.log('zone', element, element.dataset);
+		zone.data = element.dataset;
+		zone.element = element;
 
-	zones.forEach(function(zoneElement, index) {
-		
-	});
+		this.startClicked = function(event, b, c) {
+			var xhr = new XMLHttpRequest();
+			xhr.open('POST', '/zones/'+zone.data.zoneId+'/on', true); // on off delay(duration={mins})
+			xhr.onload = function(e) {
+				if (this.status == 200) {
+					console.log('success');
+					// Note: .response instead of .responseText
+					// var blob = new Blob([this.response], {type: 'image/png'});
+				}
+			};
+			xhr.send();
+		};
+		this.cancelClicked = function(event) {
+			var xhr = new XMLHttpRequest();
+			xhr.open('POST', '/zones/'+zone.data.zoneId+'/off', true); // on off delay(duration={mins})
+			xhr.onload = function(e) {
+				if (this.status == 200) {
+					console.log('success');
+					// Note: .response instead of .responseText
+					// var blob = new Blob([this.response], {type: 'image/png'});
+				}
+			};
+			xhr.send();
+		};
+		this.postponeClicked = function(event) {
+			var xhr = new XMLHttpRequest();
+			xhr.open('POST', '/zones/'+zone.data.zoneId+'/delay?duration=5', true); // on off delay(duration={mins})
+			xhr.onload = function(e) {
+				if (this.status == 200) {
+					console.log('success');
+					// Note: .response instead of .responseText
+					// var blob = new Blob([this.response], {type: 'image/png'});
+				}
+			};
+			xhr.send();
+		};
 
-};
+		var startButton = element.querySelector('button.start');
+		startButton.addEventListener('click', this.startClicked);
+		var cancelButton = element.querySelector('button.cancel');
+		cancelButton.addEventListener('click', this.cancelClicked);
+		var postponeButton = element.querySelector('button.postpone');
+		postponeButton.addEventListener('click', this.postponeClicked);
+	};
 
-var Zone = function() {
-};
+	for (var i=0,l=zones.length; i<l; i++) {
+		console.log('loop');
+		var zone = new Zone(zones[i]);
+	}
 
-Zone.prototype.
+})();
